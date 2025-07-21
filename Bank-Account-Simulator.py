3109047dd47097fc188e093ce3b3c2aded7ce79d
Rafsan Rubaet Hossain <rrhturzo2@gmail.com>
2025-07-21 13:40:31 +0600

    Add BankAccount class with deposit, withdrawal, and history methods
    - Created a BankAccount class to handle deposits, withdrawals, and track transactions
    - Added test usage at the bottom to demonstrate functionality

---
+++ Bank-Account-Simulator.py
@@ -0,0 +1,36 @@
+class BankAccount:
+    def __init__(self, owner, account_balance=0):
+        self.owner = owner
+        self._account_balance = account_balance
+        self._transaction = []
+
+    def deposit(self, amount):
+        if amount <= 0:
+            print("Deposit must be positive")
+        else:
+            self._account_balance += amount
+            self._transaction.append(f"Deposited ${amount:.2f}")
+            print(f"Deposit Successful ${amount:.2f}")
+
+    def withdrawal(self, amount):
+        if amount > self._account_balance:
+            print("Insufficient Balance")
+        elif amount <= 0:
+            print("Enter a positive number")
+        else:
+            self._account_balance -= amount
+            self._transaction.append(f"Withdrew ${amount:.2f}")
+            print(f"Withdrawal succesful ${amount:.2f}")
+        
+    def get_balance(self):
+        return (f"Account Balance {self._account_balance}")
+    
+    def transaction_history(self):
+        return self._transaction
+
+
+owner = BankAccount("asdf", 50000)
+owner.deposit(500)
+owner.withdrawal(500)
+print(owner.get_balance())
+print(owner.transaction_history())
